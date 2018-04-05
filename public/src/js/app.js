62b27a37da0aa1fbda7dd4d05786e70b24577647
Gautam Naik <gautamnaik1994@gmail.com>
2018-04-05 17:42:21 +0530

    Update app.js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,12 @@
-if('serviceWorker' in navigator){
-	navigator.serviceWorker
-	.register('/sw.js')
-	.then(function(){
-		console.log("Service Worker Registered");
-		});
+var defferedPrompt;
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function() {
+    console.log('Service Worker Registered');
+  });
 }
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('before installing prompt fired');
+  event.preventDefault();
+  defferedPrompt = event;
+  return false;
+});
