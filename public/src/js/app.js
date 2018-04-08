353f9f7741f4afe47f2ad3ea8a1e52fc87c25c7b
Gautam Naik <gautamnaik1994@gmail.com>
2018-04-09 00:40:08 +0530

    added swrvice worker

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,12 +1,24 @@
-var defferedPrompt;
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function() {
-    console.log('Service Worker Registered');
-  });
+
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
 window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('before installing prompt fired');
+  console.log('beforeinstallprompt fired');
   event.preventDefault();
-  defferedPrompt = event;
+  deferredPrompt = event;
   return false;
 });
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,38 @@
 self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker...', event);
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static').then(function(cache) {
+      console.log('[Service Worker] Precaching App Shell');
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/fetch.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+      ]);
+    }),
+  );
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker...', event);
+  console.log('[Service Worker] Activating Service Worker ...', event);
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching Something...', event);
-  event.respondWith(fetch(event.request));
+  console.log('[Service Worker] Fetching something ....', event);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request);
+      }
+    }),
+  );
 });
